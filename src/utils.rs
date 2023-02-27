e35e2e9827d2e71d6fa1a27c7b26a60660209ec8
TheAwiteb <awiteb@hotmail.com>
2023-02-27 18:37:36 +0300

    Add `create_wav_header` utility to create the header of wav file

--- src/utils.rs
+++ src/utils.rs
@@ -1,4 +1,4 @@
-use crate::{Error, Result};
+use crate::{Error, Result, SAMPLE_RATE};
 use std::fs;
 
 /// Check the file size. The maximum file size is 4 GB.
@@ -19,4 +19,28 @@ pub(crate) fn check_wav_file_size(file: &fs::File) -> Result<()> {
             ))
         })
         .unwrap_or(Ok(()))
+}
+
+/// Create the wave file header, the header is 44 bytes.
+/// ### Note
+/// - The `file_size` is the size of the file that will be encoded.
+/// - The bits per sample is 16.
+/// - The sample rate is 202860 Hz (202.86 kHz).
+/// - The number of channels is 1.
+pub(crate) fn create_wav_header(file_size: u32) -> [u8; 44] {
+    let mut header = [0; 44];
+    header[0..4].copy_from_slice(b"RIFF");
+    header[4..8].copy_from_slice(&(file_size + 36).to_le_bytes());
+    header[8..12].copy_from_slice(b"WAVE");
+    header[12..16].copy_from_slice(b"fmt ");
+    header[16..20].copy_from_slice(&16u32.to_le_bytes());
+    header[20..22].copy_from_slice(&1u16.to_le_bytes());
+    header[22..24].copy_from_slice(&1u16.to_le_bytes());
+    header[24..28].copy_from_slice(&SAMPLE_RATE.to_le_bytes());
+    header[28..32].copy_from_slice(&(SAMPLE_RATE * 2).to_le_bytes());
+    header[32..34].copy_from_slice(&2u16.to_le_bytes());
+    header[34..36].copy_from_slice(&16u16.to_le_bytes());
+    header[36..40].copy_from_slice(b"data");
+    header[40..44].copy_from_slice(&file_size.to_le_bytes());
+    header
 }
